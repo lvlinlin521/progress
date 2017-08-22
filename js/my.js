4a0993c686ae1cbb3f735a8d8b66e060d53ccf68
lvlinlin521 <1223748496@qq.com>
2017-08-22 20:35:48 +0800

    8.22 Signed-off-by: lvlinlin521 <1223748496@qq.com>

---
+++ js/my.js
@@ -0,0 +1,19 @@
+var aWrap=document.getElementById('lvXkBox');
+var oRight=document.getElementById('lvSjRight');
+var oLeft=document.getElementById('lvSjLeft');
+var boxWrap=document.getElementById('lvBoxWrap');
+// console.log(boxWrap.children.length)
+    boxWrap.children[0].className='lvShow';
+    aWrap.children[0].className='lvSjLeft';
+  for(var i=0;i<aWrap.children.length;i++){
+            aWrap.children[i].index=i;
+            aWrap.children[i].onclick=function(){
+                for(var j=0;j<boxWrap.children.length;j++){
+                    boxWrap.children[j].className='lvHidden';
+                    aWrap.children[j].className='lvSjRight';
+                }
+                aWrap.children[this.index].className='lvSjLeft';
+                boxWrap.children[this.index].className='lvShow';
+            }
+
+}
